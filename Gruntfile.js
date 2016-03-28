9e0462eda9f14906a7d785cc9e782d7cb7cf6b74
Alan Harnum <aharnum@ocadu.ca>
2016-03-28 14:05:05 -0400

    GPII-1702: grunt tasks to produce a single-file distribution (including minified version)

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,6 +15,15 @@ https://raw.githubusercontent.com/fluid-project/chartAuthoring/master/LICENSE.tx
 module.exports = function (grunt) {
     "use strict";
 
+    var licenseWrapper = function (libraryName, licenseFilePath) {
+        try {
+            return "/* " + libraryName + "\n" + grunt.file.read(licenseFilePath) + "*/\n";
+        }
+        catch (e) {
+            return null;
+        }
+    };
+
     // Project configuration.
     grunt.initConfig({
         // Project package file destination.
@@ -36,9 +45,70 @@ module.exports = function (grunt) {
                     // D3
                     {expand: true, cwd: "./node_modules/d3/", src: "**", dest: "./src/lib/d3/"},
                     // Chart Authoring
-                    {expand: true, cwd: "./node_modules/chartAuthoring/", src: "**", dest: "./src/lib/chartAuthoring/"}
+                    {expand: true, cwd: "./node_modules/chartAuthoring/", src: "**", dest: "./src/lib/chartAuthoring/"},
+                    // Flocking
+                    {expand: true, cwd: "./node_modules/flocking/", src: "**", dest: "./src/lib/flocking/"}
 
                 ]
+            }
+        },
+        concat: {
+            options: {
+                separator: ";"
+            },
+            chartAuthoringWithLicense: {
+                options: {
+                    banner: licenseWrapper("Chart Authoring Tool", "src/lib/chartAuthoring/LICENSE.txt")
+                },
+                src: "src/lib/chartAuthoring/src/js/**",
+                dest: "dist/chartAuthoring-with-license.js"
+            },
+            d3WithLicense: {
+                options: {
+                    banner: licenseWrapper("D3", "src/lib/d3/LICENSE")
+                },
+                src: ["src/lib/d3/d3.min.js"],
+                dest: "dist/d3-with-license.js"
+            },
+            flockingWithLicense: {
+                options: {
+                    banner: licenseWrapper("Flocking", "src/lib/flocking/MIT-LICENSE.txt")
+                },
+                src: ["src/lib/flocking/dist/flocking-base.js", "src/lib/flocking/src/ugens/oscillators.js", "src/lib/flocking/src/ugens/math.js",
+                "src/lib/flocking/src/ugens/envelopes.js",
+                "src/lib/flocking/src/ugens/midi.js",
+                "src/lib/flocking/src/ugens/scheduling.js"],
+                dest: "dist/flocking-with-license.js"
+            },
+            infusionWithLicense: {
+                options: {
+                    banner: licenseWrapper("Infusion", "src/lib/infusion/infusion-LICENSE.txt")
+                },
+                src: ["src/lib/infusion/infusion-custom.js"],
+                dest: "dist/infusion-with-license.js"
+            },
+            dist: {
+                src: ["dist/d3-with-license.js",
+                 "dist/infusion-with-license.js",
+                 "dist/flocking-with-license.js",
+                 "dist/chartAuthoring-with-license.js",
+                "src/js/*.js"],
+                dest: "dist/qi-frontend-full.js",
+                nonull: true
+            }
+        },
+        clean: {
+            distArtifacts: {
+                src: ["dist/*-with-license.js"]
+            }
+        },
+        uglify: {
+            options: {
+                preserveComments: "all"
+            },
+            distUglify: {
+                src: "dist/qi-frontend-full.js",
+                dest: "dist/qi-frontend-full.min.js"
             }
         }
     });
@@ -47,9 +117,13 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks("grunt-contrib-jshint");
     grunt.loadNpmTasks("grunt-jsonlint");
     grunt.loadNpmTasks("grunt-contrib-copy");
+    grunt.loadNpmTasks("grunt-contrib-concat");
+    grunt.loadNpmTasks("grunt-contrib-clean");
+    grunt.loadNpmTasks("grunt-contrib-uglify");
 
     // Custom tasks:
 
     grunt.registerTask("default", ["lint"]);
     grunt.registerTask("lint", "Apply jshint and jsonlint", ["jshint", "jsonlint"]);
+    grunt.registerTask("dist", "Build single-file distrbituion", ["concat", "uglify", "clean:distArtifacts"]);
 };
