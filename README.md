6df9cc438d2035a4be884365295d9cab93e70eaa
Alan Harnum <aharnum@ocadu.ca>
2016-03-28 14:53:43 -0400

    GPII-1702: redoing demo in light of one-file redistributable

---
+++ README.md
@@ -0,0 +1,18 @@
+# Quality Infrastructure - Frontend Demo Code
+
+## Developing
+
+- `npm install`
+- `grunt copy:frontEndDependencies`
+
+## Building a Redistributable Single File Version
+
+- `npm install`
+- `grunt copy:frontEndDependencies`
+- `grunt dist`
+
+This produces single-file versons in the /dist directory
+
+## The Demo
+
+The demo in `/demos` assumes a single-file distribution has been built and includes it
--- demos/src/js/qi-frontend-demo.js
+++ demos/src/js/qi-frontend-demo.js
@@ -1,3 +1,84 @@
 "use strict";
 
-fluid.registerNamespace("gpii.qualityInfrastructure.frontEnd");
+fluid.registerNamespace("gpii.qualityInfrastructure.frontEnd.demo");
+
+// A panels configuration object looks like this:
+// {
+//   repo: "gpii/universal",
+//   buildsContainer: "gpiic-metrics-builds",
+//   testsContainer: "gpiic-metrics-tests",
+//   commitsContainer: "gpiic-metrics-commits",
+//   contributorsContainer: "gpiic-metrics-contributors"
+// }
+
+gpii.qualityInfrastructure.frontEnd.createPanels = function(panelsConfig) {
+    var repo = panelsConfig.repo;
+    var commitsContainer = panelsConfig.commitsContainer;
+    var contributorsContainer = panelsConfig.contributorsContainer;
+
+    var commitsEndpoint = "http://localhost:3000/a/" + repo + "/commits";
+
+    var contributorsEndpoint = "http://localhost:3000/a/" + repo + "/contributors";
+
+    var commitsPanel = gpii.qualityInfrastructure.frontEnd.commitsMetricsPanel(commitsContainer, {
+        components: {
+            jsonpLoader: {
+                options: {
+                    jsonpOptions: {
+                        url: commitsEndpoint
+                    }
+                }
+            }
+        }
+    });
+
+    var contributorsPanel = gpii.qualityInfrastructure.frontEnd.contributorsMetricsPanel(contributorsContainer, {
+        components: {
+            jsonpLoader: {
+                options: {
+                    jsonpOptions: {
+                        url: contributorsEndpoint
+                    }
+                }
+            }
+        }
+    });
+
+    return {
+        commitsPanel: commitsPanel,
+        contributorsPanel: contributorsPanel
+    };
+
+};
+
+gpii.qualityInfrastructure.frontEnd.demo.animate = function(commitsPanel, contributorsPanel) {
+    // var dayZooms = [30, 60, 180, 365, 730, 1040, 2080];
+
+    var originalMetricsEndDate = fluid.copy( contributorsPanel.model.currentEventsDataViewSettings.metricsEndDate);
+    var nextMetricsEndDate;
+
+    var changeView = function() {
+        try {
+            var currentMetricsEndDate = contributorsPanel.model.currentEventsDataViewSettings.metricsEndDate;
+
+            nextMetricsEndDate = new Date(currentMetricsEndDate);
+
+            nextMetricsEndDate.setDate(currentMetricsEndDate.getDate() - 30);
+
+            commitsPanel.applier.change("currentEventsDataViewSettings.metricsEndDate", nextMetricsEndDate);
+
+            contributorsPanel.applier.change("currentEventsDataViewSettings.metricsEndDate", nextMetricsEndDate);
+        }
+        // An error means we've moved past the start of the data coverage
+        // range, so should reset to the start
+        catch(e) {
+            commitsPanel.applier.change("currentEventsDataViewSettings.metricsEndDate", originalMetricsEndDate);
+
+            contributorsPanel.applier.change("currentEventsDataViewSettings.metricsEndDate", originalMetricsEndDate);
+
+        }
+
+    };
+
+    window.setInterval(changeView, 3000);
+};
