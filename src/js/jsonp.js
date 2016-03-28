48fd11000e968024b67b55e133efd4a75c99809b
Alan Harnum <aharnum@ocadu.ca>
2016-03-28 13:28:57 -0400

    GPII-1681: add license statements

--- src/js/jsonp.js
+++ src/js/jsonp.js
@@ -1,3 +1,14 @@
+/*
+Copyright 2016 OCAD University
+
+Licensed under the Educational Community License (ECL), Version 2.0 or the New
+BSD license. You may not use this file except in compliance with one these
+Licenses.
+
+You may obtain a copy of the ECL 2.0 License and BSD License at
+https://raw.githubusercontent.com/waharnum/qi-dashboard-frontend-demo/GPII-1681/LICENSE.txt
+*/
+
 (function ($, fluid) {
 
     "use strict";
