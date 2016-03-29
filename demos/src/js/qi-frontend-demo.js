750c5d99b6920cf47fdc99e746ae78bbb110ee7c
Alan Harnum <waharnum@gmail.com>
2016-03-29 09:20:33 -0400

    GPII-1702: demo updates + createPanels updates

--- demos/src/js/qi-frontend-demo.js
+++ demos/src/js/qi-frontend-demo.js
@@ -1,35 +1,39 @@
-"use strict";
+(function ($, fluid) {
 
-fluid.registerNamespace("gpii.qualityInfrastructure.frontEnd.demo");
+    "use strict";
 
-gpii.qualityInfrastructure.frontEnd.demo.animate = function(commitsPanel, contributorsPanel) {
-    // var dayZooms = [30, 60, 180, 365, 730, 1040, 2080];
+    fluid.registerNamespace("gpii.qualityInfrastructure.frontEnd.demo");
 
-    var originalMetricsEndDate = fluid.copy( contributorsPanel.model.currentEventsDataViewSettings.metricsEndDate);
-    var nextMetricsEndDate;
+    gpii.qualityInfrastructure.frontEnd.demo.animate = function(commitsPanel, contributorsPanel) {
+        // var dayZooms = [30, 60, 180, 365, 730, 1040, 2080];
 
-    var changeView = function() {
-        try {
-            var currentMetricsEndDate = contributorsPanel.model.currentEventsDataViewSettings.metricsEndDate;
+        var originalMetricsEndDate = fluid.copy( contributorsPanel.model.currentEventsDataViewSettings.metricsEndDate);
+        var nextMetricsEndDate;
 
-            nextMetricsEndDate = new Date(currentMetricsEndDate);
+        var changeView = function() {
+            try {
+                var currentMetricsEndDate = contributorsPanel.model.currentEventsDataViewSettings.metricsEndDate;
 
-            nextMetricsEndDate.setDate(currentMetricsEndDate.getDate() - 30);
+                nextMetricsEndDate = new Date(currentMetricsEndDate);
 
-            commitsPanel.applier.change("currentEventsDataViewSettings.metricsEndDate", nextMetricsEndDate);
+                nextMetricsEndDate.setDate(currentMetricsEndDate.getDate() - 30);
 
-            contributorsPanel.applier.change("currentEventsDataViewSettings.metricsEndDate", nextMetricsEndDate);
-        }
-        // An error means we've moved past the start of the data coverage
-        // range, so should reset to the start
-        catch(e) {
-            commitsPanel.applier.change("currentEventsDataViewSettings.metricsEndDate", originalMetricsEndDate);
+                commitsPanel.applier.change("currentEventsDataViewSettings.metricsEndDate", nextMetricsEndDate);
 
-            contributorsPanel.applier.change("currentEventsDataViewSettings.metricsEndDate", originalMetricsEndDate);
+                contributorsPanel.applier.change("currentEventsDataViewSettings.metricsEndDate", nextMetricsEndDate);
+            }
+            // An error means we've moved past the start of the data coverage
+            // range, so should reset to the start
+            catch(e) {
+                commitsPanel.applier.change("currentEventsDataViewSettings.metricsEndDate", originalMetricsEndDate);
 
-        }
+                contributorsPanel.applier.change("currentEventsDataViewSettings.metricsEndDate", originalMetricsEndDate);
 
+            }
+
+        };
+
+        window.setInterval(changeView, 3000);
     };
 
-    window.setInterval(changeView, 3000);
-};
+})(jQuery, fluid);
--- src/js/createPanels.js
+++ src/js/createPanels.js
@@ -9,56 +9,76 @@ You may obtain a copy of the ECL 2.0 License and BSD License at
 https://raw.githubusercontent.com/waharnum/qi-dashboard-frontend-demo/GPII-1681/LICENSE.txt
 */
 
-fluid.registerNamespace("gpii.qualityInfrastructure.frontEnd");
+(function ($, fluid) {
 
-// A panels configuration object looks like this:
-// {
-//   repo: "gpii/universal",
-//   buildsContainer: "gpiic-metrics-builds",
-//   testsContainer: "gpiic-metrics-tests",
-//   commitsContainer: "gpiic-metrics-commits",
-//   contributorsContainer: "gpiic-metrics-contributors"
-// }
+    "use strict";
 
-gpii.qualityInfrastructure.frontEnd.createPanels = function(panelsConfig) {
-    var repo = panelsConfig.repo;
-    var commitsContainer = panelsConfig.commitsContainer;
-    var contributorsContainer = panelsConfig.contributorsContainer;
+    fluid.registerNamespace("gpii.qualityInfrastructure.frontEnd");
 
-    var commitsEndpoint = "http://localhost:3000/a/" + repo + "/commits";
+    // A panels configuration object looks like this:
+    // {
+    //   qualityInfrastructureHost: host address of QI instance, ex: "http://localhost:3000",
+    //   apiVersion: alphabetical version of the QI API to use, ex: "a"
+    //   repo: repo to retrieve results for, ex: "gpii/universal",
+    //   buildsContainer: selector for container for build metrics panel, ex: ".gpiic-metrics-builds",
+    //   testsContainer: selector for container for tests metrics panel, ex: ".gpiic-metrics-tests",
+    //   commitsContainer: selector for container for commits metrics, ex: ".gpiic-metrics-commits",
+    //   contributorsContainer: selector for container for contributors metrics panel, ex: ".gpiic-metrics-contributors"
+    // }
 
-    var contributorsEndpoint = "http://localhost:3000/a/" + repo + "/contributors";
+    gpii.qualityInfrastructure.frontEnd.createPanels = function(panelsConfig) {
+        var qualityInfrastructureHost = panelsConfig.qualityInfrastructureHost;
+        var apiVersion = panelsConfig.apiVersion;
+        var repo = panelsConfig.repo;
+        var commitsContainer = panelsConfig.commitsContainer;
+        var contributorsContainer = panelsConfig.contributorsContainer;
 
-    var commitsPanel = gpii.qualityInfrastructure.frontEnd.commitsMetricsPanel(commitsContainer, {
-        components: {
-            jsonpLoader: {
-                options: {
-                    jsonpOptions: {
-                        url: commitsEndpoint
-                    }
-                }
-            }
-        }
-    });
+        var commitsPanel = gpii.qualityInfrastructure.frontEnd.createCommitsPanel(qualityInfrastructureHost, apiVersion, repo, commitsContainer);
 
-    var contributorsPanel = gpii.qualityInfrastructure.frontEnd.contributorsMetricsPanel(contributorsContainer, {
-        components: {
-            jsonpLoader: {
-                options: {
-                    jsonpOptions: {
-                        url: contributorsEndpoint
-                    }
-                }
-            }
-        }
-    });
+        var contributorsPanel = gpii.qualityInfrastructure.frontEnd.createContributorsPanel(qualityInfrastructureHost, apiVersion, repo, contributorsContainer);
 
-    // Returns a data object with references to the created panels, in case
-    // the implenentation needs to do something further with them
+        // Returns a data object with references to the created panels, in case
+        // the implenentation needs to do something further with them
 
-    return {
-        commitsPanel: commitsPanel,
-        contributorsPanel: contributorsPanel
+        return {
+            commitsPanel: commitsPanel,
+            contributorsPanel: contributorsPanel
+        };
+
     };
 
-};
+    gpii.qualityInfrastructure.frontEnd.getEndpointURL = function(qualityInfrastructureHost, apiVersion, repo, endpoint) {
+        return qualityInfrastructureHost + "/" + apiVersion + "/" + repo + "/" + endpoint;
+    };
+
+    gpii.qualityInfrastructure.frontEnd.createCommitsPanel = function (qualityInfrastructureHost, apiVersion, repo, container) {
+        var commitsEndpoint = gpii.qualityInfrastructure.frontEnd.getEndpointURL(qualityInfrastructureHost, apiVersion, repo, "commits");
+        return gpii.qualityInfrastructure.frontEnd.commitsMetricsPanel(container, {
+            components: {
+                jsonpLoader: {
+                    options: {
+                        jsonpOptions: {
+                            url: commitsEndpoint
+                        }
+                    }
+                }
+            }
+        });
+    };
+
+    gpii.qualityInfrastructure.frontEnd.createContributorsPanel = function (qualityInfrastructureHost, apiVersion, repo, container) {
+        var contributorsEndpoint = qualityInfrastructureHost + "/" + apiVersion + "/" + repo + "/contributors";
+        return gpii.qualityInfrastructure.frontEnd.contributorsMetricsPanel(container, {
+            components: {
+                jsonpLoader: {
+                    options: {
+                        jsonpOptions: {
+                            url: contributorsEndpoint
+                        }
+                    }
+                }
+            }
+        });
+    };
+
+})(jQuery, fluid);
