34c41aea1928b58677747bb8d9dacdef193e529f
Alan Harnum <aharnum@ocadu.ca>
2016-04-01 09:51:58 -0400

    GPII-1702: add a data range changer

--- demos/src/js/qi-frontend-demo.js
+++ demos/src/js/qi-frontend-demo.js
@@ -4,6 +4,8 @@
 
     fluid.registerNamespace("gpii.qualityInfrastructure.frontEnd.demo");
 
+    gpii.qualityInfrastructure.frontEnd.demo.daysToZoom = 180;
+
     gpii.qualityInfrastructure.frontEnd.demo.getURLParamValue = function (param) {
         var params={};window.location.search.replace(/[?&]+([^=&]+)=([^&]*)/gi,function(str,key,value){params[key] = value;});
         return params[param];
@@ -33,19 +35,43 @@
 
 
         $("#gpiic-metrics-back").click(function (e) {
-            gpii.qualityInfrastructure.frontEnd.demo.moveView(commitsPanel, -180);
-            gpii.qualityInfrastructure.frontEnd.demo.moveView(contributorsPanel, -180);
+            gpii.qualityInfrastructure.frontEnd.demo.moveView(commitsPanel, -gpii.qualityInfrastructure.frontEnd.demo.daysToZoom);
+            gpii.qualityInfrastructure.frontEnd.demo.moveView(contributorsPanel, -gpii.qualityInfrastructure.frontEnd.demo.daysToZoom);
             e.preventDefault();
         });
 
         $("#gpiic-metrics-forward").click(function (e) {
-            gpii.qualityInfrastructure.frontEnd.demo.moveView(commitsPanel, 180);
-            gpii.qualityInfrastructure.frontEnd.demo.moveView(contributorsPanel, 180);
+            gpii.qualityInfrastructure.frontEnd.demo.moveView(commitsPanel, gpii.qualityInfrastructure.frontEnd.demo.daysToZoom);
+            gpii.qualityInfrastructure.frontEnd.demo.moveView(contributorsPanel, gpii.qualityInfrastructure.frontEnd.demo.daysToZoom);
+            e.preventDefault();
+        });
+
+        $("#gpiic-metrics-zoom-sixMonths").click(function (e) {
+            gpii.qualityInfrastructure.frontEnd.demo.changeZoom(commitsPanel, 180);
+            gpii.qualityInfrastructure.frontEnd.demo.changeZoom(contributorsPanel, 180);
+            e.preventDefault();
+        });
+
+        $("#gpiic-metrics-zoom-oneYear").click(function (e) {
+            gpii.qualityInfrastructure.frontEnd.demo.changeZoom(commitsPanel, 365);
+            gpii.qualityInfrastructure.frontEnd.demo.changeZoom(contributorsPanel, 365);
+            e.preventDefault();
+        });
+
+        $("#gpiic-metrics-zoom-threeMonths").click(function (e) {
+            gpii.qualityInfrastructure.frontEnd.demo.changeZoom(commitsPanel, 90);
+            gpii.qualityInfrastructure.frontEnd.demo.changeZoom(contributorsPanel, 90);
+            e.preventDefault();
+        });
+
+        $("#gpiic-metrics-zoom-oneMonth").click(function (e) {
+            gpii.qualityInfrastructure.frontEnd.demo.changeZoom(commitsPanel, 30);
+            gpii.qualityInfrastructure.frontEnd.demo.changeZoom(contributorsPanel, 30);
             e.preventDefault();
         });
 
         if (shouldAnimate) {
-            gpii.qualityInfrastructure.frontEnd.demo.animate(commitsPanel, contributorsPanel, 180);
+            gpii.qualityInfrastructure.frontEnd.demo.animate(commitsPanel, contributorsPanel, gpii.qualityInfrastructure.frontEnd.demo.daysToZoom);
             }
 
     };
@@ -58,6 +84,11 @@
         nextMetricsEndDate.setDate(currentMetricsEndDate.getDate() + rollDays);
 
         panel.applier.change("currentEventsDataViewSettings.metricsEndDate", nextMetricsEndDate);
+    };
+
+    gpii.qualityInfrastructure.frontEnd.demo.changeZoom = function (panel, daysToZoom) {
+        gpii.qualityInfrastructure.frontEnd.demo.daysToZoom = daysToZoom;
+        panel.applier.change("currentEventsDataViewSettings.daysBack", daysToZoom);
     };
 
     gpii.qualityInfrastructure.frontEnd.demo.moveView = function (panel, daysToScroll) {
