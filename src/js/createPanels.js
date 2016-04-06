3757aed545570dc05fe779a35a780c9587b7fcfa
Alan Harnum <waharnum@gmail.com>
2016-04-06 09:21:39 -0400

    GPII-1702: request all scripts (including JSONP) over https by default

--- src/js/createPanels.js
+++ src/js/createPanels.js
@@ -16,7 +16,7 @@ https://raw.githubusercontent.com/waharnum/qi-dashboard-frontend-demo/GPII-1681/
     fluid.registerNamespace("gpii.qualityInfrastructure.frontEnd");
 
     gpii.qualityInfrastructure.frontEnd.qualityInfrastructureSettings = {
-        host: "http://qi-backend.gpii.net",
+        host: "https://qi-backend.gpii.net",
         apiVersion: "a",
         // For testing without a backend service
         endpointSuffix: ""
